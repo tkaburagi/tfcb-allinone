3e5ce3b91b0edb6c4cda30ac9b72879666a28657
tkaburagi <t.kaburagi@me.com>
2021-05-18 18:31:45 +0900

    aws-infra

--- aws-infra/tf/main.tf
+++ aws-infra/tf/main.tf
@@ -27,7 +27,6 @@ resource "aws_security_group" "allow_tls" {
     to_port          = 0
     protocol         = "-1"
     cidr_blocks      = ["0.0.0.0/0"]
-    ipv6_cidr_blocks = ["::/0"]
   }
 
   tags = {
@@ -46,8 +45,6 @@ resource "aws_vpc" "my_vpc" {
 resource "aws_subnet" "my_subnet" {
   vpc_id            = aws_vpc.my_vpc.id
   cidr_block        = "172.16.10.0/24"
-  availability_zone = "us-west-2a"
-
   tags = {
     Name = "tf-example"
   }
