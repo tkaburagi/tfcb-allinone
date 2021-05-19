822d1b497ed9df4a20f0c414b79d4f63d2cc709f
tkaburagi <t.kaburagi@me.com>
2021-05-19 10:28:43 +0900

    modified cidr egress

--- aws-infra/tf/main.tf
+++ aws-infra/tf/main.tf
@@ -25,7 +25,7 @@ resource "aws_security_group" "allow_tls" {
     from_port        = 0
     to_port          = 0
     protocol         = "-1"
-    cidr_blocks      = ["0.0.0.0/0"]
+    cidr_blocks      = ["172.16.10.0/24"]
   }
 
   tags = {
