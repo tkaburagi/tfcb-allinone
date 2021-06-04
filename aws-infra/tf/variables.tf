a2227690eafe08a85387814dd2d72b72f61a3eb4
Kabu <t.kaburagi@me.com>
2021-06-04 13:24:51 +0900

    インスタンスの数を5に変更

--- aws-infra/tf/variables.tf
+++ aws-infra/tf/variables.tf
@@ -3,7 +3,7 @@ variable "secret_key" {}
 variable "region" {}
 variable "ami" {}
 variable "hello_tf_instance_count" {
-  default = 1
+  default = 
 }
 variable "hello_tf_instance_type" {
   default = "t2.large"
