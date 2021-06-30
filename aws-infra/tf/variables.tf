34ea77f7e7b589c2c0e6046c90ce0745d808b1a7
Kabu <t.kaburagi@me.com>
2021-06-30 16:46:40 +0900

    Update variables.tf

--- aws-infra/tf/variables.tf
+++ aws-infra/tf/variables.tf
@@ -3,7 +3,7 @@ variable "secret_key" {}
 variable "region" {}
 variable "ami" {}
 variable "hello_tf_instance_count" {
-  default = 5
+  default = 1
 }
 variable "hello_tf_instance_type" {
   default = "t2.large"
