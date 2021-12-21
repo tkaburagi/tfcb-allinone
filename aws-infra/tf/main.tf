0b6a47b486b120faff27cb7feca93696021f5d86
Kabu <t.kaburagi@me.com>
2021-12-21 15:51:45 +0900

    Update main.tf

--- aws-infra/tf/main.tf
+++ aws-infra/tf/main.tf
@@ -24,7 +24,7 @@ resource "aws_security_group" "allow_tls" {
     from_port   = 0
     to_port     = 0
     protocol    = "-1"
-    cidr_blocks = ["172.16.0.0/16"]
+    cidr_blocks = ["0.0.0.0/0"]
   }
 
   tags = {
